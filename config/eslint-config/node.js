b1fb940fdc73e86a524a4177168c4c9ec70eb182
Paulo Borges <dev.pauloborges@gmail.com>
2026-01-29 12:57:17 -0300

    chore: auth package setup

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,8 +1,16 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['@rocketseat/eslint-config/node'],
-  plugins: ['simple-import-sort'],
-  rules: {
-    'simple-import-sort/imports': 'error'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+    },
   }
-}
+)
---
+++ packages/auth/eslint.config.js
@@ -0,0 +1,3 @@
+import nodeConfig from '@saas/eslint-config/node.js'
+
+export default [...nodeConfig]
